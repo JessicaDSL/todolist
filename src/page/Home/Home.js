90c04cb35750767864266bc6d8bc8c5e4184dcd1
JessicaLima <jessikurt@gmail.com>
2022-02-01 19:12:00 -0300

    feat: add list and TodoList for the Home on pages

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import AddQuickTodo from "../../components/AddQuickTodo";
+import TodoList from "../../components/TodoList/TodoList";
+import { TodoContext } from "../../Context";
 import { Container } from "./styles";
 
 const Home = () => {
+  const { list } = useContext(TodoContext);
+
   return (
     <Container>
       <AddQuickTodo />
+      {list.length <= 0 ? "Ainda não possui itens na lista" : <TodoList />}
     </Container>
   );
 };
