85c80837393156f214b65f426f7d2b23475ae9e4
JessicaLima <jessikurt@gmail.com>
2022-02-01 19:00:11 -0300

    feat: created a list for todo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TodoContext } from "../../Context";
+import TodoItem from "../TodoItem/TodoItem";
 import { Container } from "./styles";
 
 const TodoList = () => {
-  return <Container></Container>;
+  const { list } = useContext(TodoContext);
+
+  return (
+    <Container>
+      {list.map((item) => (
+        <TodoItem item={item} />
+      ))}
+    </Container>
+  );
 };
 
 export default TodoList;
