98a0bd08783bc16d4c28e8d5ce7af52075ea0567
JessicaLima <jessikurt@gmail.com>
2022-02-01 19:01:31 -0300

    feat: crete content for TodoItem

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const TodoItem = () => {
+const TodoItem = ({ item }) => {
   return (
     <div>
-      
+      <h2>
+        {item.work} <span>seta</span>
+      </h2>
+      <button>Editar</button>
+      <button>Remover</button>
     </div>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
