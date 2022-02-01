ff6a67f5b125422c632f8d3f8be3362680733f6e
JessicaLima <jessikurt@gmail.com>
2022-02-01 13:09:00 -0300

    feat: created ContextAPI

---
+++ src/Context/index.js
@@ -0,0 +1,7 @@
+import React, { createContext } from "react";
+
+export const TodoContext = createContext({});
+
+export const TodoProvider = ({ children }) => {
+  return <TodoContext.Provider value={{}}>{children}</TodoContext.Provider>;
+};
