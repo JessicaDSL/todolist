8a5700422147dd8c6e9d0f78a93efdf8f8ab537c
JessicaLima <jessikurt@gmail.com>
2022-02-01 17:38:12 -0300

    feat: create a form for the component

--- src/components/AddQuickTodo/AddQuickTodo.js
+++ src/components/AddQuickTodo/AddQuickTodo.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { TodoContext } from "../../Context";
 
 const AddQuickTodo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [task, setTask] = useState([""]);
 
-export default AddQuickTodo
+  const { handleAddItem } = useContext(TodoContext);
+
+  function getTask(e) {
+    const newText = e.target.value;
+    setTask(newText);
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+        onChange={getTask}
+        value={task}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          handleAddItem(task);
+          setTask("");
+        }}
+      >
+        ADD
+      </button>
+    </form>
+  );
+};
+
+export default AddQuickTodo;
