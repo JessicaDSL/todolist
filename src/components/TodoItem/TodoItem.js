d91581375b7be70882b4ec89a1ab7f4f929362f5
JessicaLima <jessikurt@gmail.com>
2022-02-02 20:28:01 -0300

    feat: add new icon,change modal and modal button

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,55 +1,76 @@
 import React, { useState, useContext, useEffect } from "react";
 import { TodoContext } from "../../Context";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import ModalTodo from "../ModalTodo";
+import { Container, Span, TaskStatus, Button } from "./styles";
 
 const TodoItem = ({ item }) => {
   const { handleDelete } = useContext(TodoContext);
 
   const [state, setState] = useState(false);
-  const [active, setActive] = useState("");
   const [open, setOpen] = useState(false);
+  const [hasDescription, setHasDescription] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   useEffect(() => {
-    state ? setActive("Completed!") : setActive("Active");
-  }, [state]);
+    item.description === undefined
+      ? setHasDescription(false)
+      : setHasDescription(true);
+  }, [item.description]);
 
   const style = {
     position: "absolute",
-    top: "50%",
+    top: "55%",
     left: "50%",
+    padding: "0",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: 300,
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
-    p: 4,
+    p: 1,
+    m: 0,
   };
 
+  console.log(item.description);
   return (
-    <div>
-      <input type="checkbox" onClick={() => setState(true)} />
-      {active}
-      <h2>
-        {item.work} <span>seta</span>
-      </h2>
-      <button onClick={handleOpen}>Editar</button>
-      <button onClick={() => handleDelete(item)}>Remover</button>
+    <Container state={state}>
+      <TaskStatus>
+        <input type="checkbox" onClick={() => setState(true)} />
+        <span></span>
+      </TaskStatus>
+
+      <p>
+        {item.work}{" "}
+        <Span hasDescription={hasDescription} onClick={handleOpen}>
+          <ArrowForwardIcon />
+        </Span>
+      </p>
+      <div>
+        <button onClick={handleOpen} style={{ backgroundColor: "#4C86A8" }}>
+          EDIT
+        </button>
+        <button
+          onClick={() => handleDelete(item)}
+          style={{ backgroundColor: "#f1123b" }}
+        >
+          DELETE
+        </button>
+      </div>
       <Modal
         open={open}
-        onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <ModalTodo item={item} handleClose={handleClose} />
-          <button onClick={handleClose}>Salvar as alterações</button>
+          <ModalTodo item={item} />
+          <Button onClick={handleClose}>Salvar as alterações</Button>
         </Box>
       </Modal>
-    </div>
+    </Container>
   );
 };
 
