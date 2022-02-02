b5552f5ed21164cfb6c24c11eaef2c710a4ae6bb
JessicaLima <jessikurt@gmail.com>
2022-02-02 20:25:32 -0300

    feat: add a form and a edit button on component

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,24 +1,33 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useState } from "react";
+import { Form, Title, DescriptionTask } from "./styles";
 
 const TodoForm = ({ item }) => {
+  const [state, setState] = useState(false);
+
   return (
-    <Container>
-      <form>
-        <h1
-          contentEditable="true"
+    <Form>
+      <h4>Title:</h4>
+      <Title state={state}>
+        <p
+          contentEditable={state}
           onKeyDown={(e) => (item.work = e.target.innerText)}
         >
           {item.work}
-        </h1>
+        </p>
+        <button type="button" onClick={() => setState(true)}>
+          EDIT
+        </button>
+      </Title>
+      <DescriptionTask>
+        <h4>Descrição:</h4>
         <p
           contentEditable="true"
           onKeyDown={(e) => (item["description"] = e.target.innerText)}
         >
           {item.description}
         </p>
-      </form>
-    </Container>
+      </DescriptionTask>
+    </Form>
   );
 };
 
