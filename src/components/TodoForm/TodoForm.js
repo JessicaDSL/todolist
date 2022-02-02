57c44256638016b0a52a25c12388cfa014c22fde
JessicaLima <jessikurt@gmail.com>
2022-02-02 13:09:47 -0300

    feat: create a content for TodoForm

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Container } from "./styles";
 
-const TodoForm = () => {
+const TodoForm = ({ item }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+      <form>
+        <h1
+          contentEditable="true"
+          onKeyDown={(e) => (item.work = e.target.innerText)}
+        >
+          {item.work}
+        </h1>
+        <p>{item.description}</p>
 
-export default TodoForm
+        <textarea
+          rows="5"
+          cols="33"
+          onChange={(e) => (item["description"] = e.target.value)}
+        />
+      </form>
+    </Container>
+  );
+};
+
+export default TodoForm;
