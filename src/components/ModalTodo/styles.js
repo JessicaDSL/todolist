2ba9d9268f23150dac8defbbad16378305298df3
JessicaLima <jessikurt@gmail.com>
2022-02-21 19:25:22 -0300

    feat: create a id an closed button on modal

--- src/components/ModalTodo/styles.js
+++ src/components/ModalTodo/styles.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+background-color: rgba(0, 0, 0, 0.8);
+  width: 100vw;
+  height: 100vh;
+  z-index: 10;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+`;
+
+export const Content = styled.div`
+position: fixed;
+background: #fff;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
