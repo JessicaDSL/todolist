0e76ebce03c9d975dc7d20375bf158e407d27aa3
JessicaLima <jessikurt@gmail.com>
2022-02-22 01:30:33 -0300

    refactored: refactor todoForm component

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { Form, Title, DescriptionTask } from "./styles";
+import { Form, Title, DescriptionTask, Btns } from "./styles";
 
-const TodoForm = ({ item }) => {
+const TodoForm = ({ task }) => {
   return (
-    <Form>
-      <h4>Title:</h4>
-      <Title>
-        <p
-          contentEditable="true"
-          onKeyDown={(e) => (item.title = e.target.innerText)}
-        >
-          {item.title}
-        </p>
+    <Form suppressContentEditableWarning = { true } >
+      Title:
+      <Title
+        contentEditable="true"
+        onKeyDown={(e) => (task.title = e.target.innerText)}
+      >
+        {task.title}
       </Title>
-      <DescriptionTask>
-        <h4>Descrição:</h4>
-        <p
-          contentEditable="true"
-          onKeyDown={(e) => item.description = e.target.innerText}
-        >
-          {item.description}
-        </p>
+      Descrição:
+      <DescriptionTask
+        contentEditable="true"
+        suppressContentEditableWarning="true"
+        onKeyDown={(e) => (task.description = e.target.innerText)}
+      >
+        {task.description}
       </DescriptionTask>
+      <Btns>
+        <button style={{ backgroundColor: "#ff6d54" }}>DO!</button>
+        <button style={{ backgroundColor: "#00c2ea" }}>DOING!</button>
+        <button style={{ backgroundColor: "#00de3d" }}>DONE!</button>
+      </Btns>
     </Form>
   );
 };
--- src/components/TodoForm/styles.js
+++ src/components/TodoForm/styles.js
@@ -2,51 +2,57 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   border-radius: 4px;
-  width: 250px;
-  height: 170px;
-  background: white;
-  color: white;
+  width: 310px;
+  min-height: 220px;
+ // background: red;
+  font-weight: 700;
+  font-size: large;
   z-index: 10;
   border-radius: 16px;
+  padding: 0 .8rem;
   box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.04);
-  h4 {
-    color: #303030;
-  }
+  color: #303030;
 `;
 
 export const Title = styled.div`
   display: flex;
-  p {
-    max-height: 4rem;
-    display: flex;
-    align-items: center;
-    overflow-y: scroll;
-    border: 1px solid #c8cacc;
-    border-radius: 4px 0px 0 4px;
-    width: 82%;
-    padding: 0 0 0 0.4rem;
-    font-size: medium;
-    font-weight: 500;
-    color: #777879;
-  }
-  button {
-    border-radius: ${({ state }) => (state ? "0px 4px 4px 0" : "none")};
-    border: 1px solid #fd7294;
-    background: #fd7294;
-    color: #fff;
-    padding: 0 0.6rem;
-    height: 2rem;
-  }
+  min-height: 2rem;
+  max-height: 4rem;
+  display: flex;
+  align-items: center;
+  overflow-y: scroll;
+  border: 1px solid #c8cacc;
+  border-radius: 4px;
+  width: 82%;
+  padding: 0 0 0 0.4rem;
+  margin: .4rem 0 1.5rem 0;
+  font-size: medium;
+  font-weight: 500;
+  color: #777879;
 `;
 
 export const DescriptionTask = styled.div`
-  margin: 0.8rem 0;
-  p {
-    padding: 0.1rem;
-    border: 1px solid #c8cacc;
-    border-radius: 4px;
-    min-height: 4rem;
-    max-height: 7rem;
-    overflow-y: scroll;
-  }
+  margin: 0.4rem 0;
+  padding: 0.4rem;
+  border: 1px solid #c8cacc;
+  border-radius: 4px;
+  min-height: 8rem;
+  max-height: 10rem;
+  overflow-y: scroll;
+  font-weight: 500;
 `;
+
+export const Btns = styled.div`
+width: 100%;
+//background: blue;
+display: flex;
+  button {
+    flex-grow: 1; 
+    height: 2.2rem;
+    border-radius: 6px;
+    margin: 1rem .2rem 1.1rem;
+    color: #fff;
+    font-weight: 600;
+    border: none;
+  }
+`
