4446d99657aa76f110b9ac7831b6cddf5e1e52a1
JessicaLima <jessikurt@gmail.com>
2022-02-22 01:32:39 -0300

    refactored: refactor context api component

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import { toast } from "react-toastify";
 
 import { v4 as uuidv4 } from "uuid";
@@ -6,11 +6,20 @@ export const TodoContext = createContext({});
 
 export const TodoProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  function openModal() {
+    setIsModalVisible(true)
+  }
+  
+  function closeModal() {
+    setIsModalVisible(false)
+  }
 
   function handleAddTask(taskTitle) {
     const id = uuidv4();
     taskTitle.length === 0
-      ? toast.error("Favor digitar uma tarefa!", {
+      ? toast.error("Please enter a task!", {
           theme: "dark",
         })
       : setTasks([...tasks, { id: id, title: taskTitle }]);
@@ -21,7 +30,7 @@ export const TodoProvider = ({ children }) => {
   }
 
   return (
-    <TodoContext.Provider value={{ handleAddTask, handleDeleteTask, tasks }}>
+    <TodoContext.Provider value={{ handleAddTask, handleDeleteTask, closeModal, openModal, tasks, isModalVisible }}>
       {children}
     </TodoContext.Provider>
   );
