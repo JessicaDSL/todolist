55949b472107b0a3d382da0797523c4a2f769e46
JessicaLima <jessikurt@gmail.com>
2022-02-23 13:32:11 -0300

    feat: add status button

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,29 +1,83 @@
-import React from "react";
-import { Form, Title, DescriptionTask, Btns } from "./styles";
+import React, { useState, useContext, useEffect } from "react";
+import {
+  Form,
+  Title,
+  DescriptionTask,
+  BtnsStatusTask,
+  ButtonSave,
+  DoButton,
+  DoingButton,
+  DoneButton,
+} from "./styles";
+
+import { TodoContext } from "../../Context";
 
 const TodoForm = ({ task }) => {
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [taskStatus, setTaskStatus] = useState("");
+  const [doStatus, setDoStatus] = useState(false);
+  const [doingStatus, setDoingStatus] = useState(false);
+  const [doneStatus, setDoneStatus] = useState(false);
+  const { closeModal } = useContext(TodoContext);
+
+  function handleInfoModal() {
+    task.title = newTitle || task.title;
+    task.description = newDescription || task.description;
+    task.status = taskStatus;
+    
+    closeModal();
+  }
+
+  useEffect(() => {
+    
+    setTaskStatus(task.status) 
+  }, [task.status])
+
+  useEffect(() => {
+    taskStatus === "do" ? setDoStatus(true) : setDoStatus(false);
+    taskStatus === "doing" ? setDoingStatus(true) : setDoingStatus(false);
+    taskStatus === "done" ? setDoneStatus(true) : setDoneStatus(false);
+  }, [taskStatus]);
+
   return (
-    <Form suppressContentEditableWarning = { true } >
-      Title:
-      <Title
-        contentEditable="true"
-        onKeyDown={(e) => (task.title = e.target.innerText)}
-      >
+    <Form>
+      <label>Title:</label>
+      <Title onChange={(e) => setNewTitle(e.target.value)} rows="1">
         {task.title}
       </Title>
-      Descrição:
-      <DescriptionTask
-        contentEditable="true"
-        suppressContentEditableWarning="true"
-        onKeyDown={(e) => (task.description = e.target.innerText)}
-      >
+      <label>Descrição:</label>
+      <DescriptionTask onChange={(e) => setNewDescription(e.target.value)}>
         {task.description}
       </DescriptionTask>
-      <Btns>
-        <button style={{ backgroundColor: "#ff6d54" }}>DO!</button>
-        <button style={{ backgroundColor: "#00c2ea" }}>DOING!</button>
-        <button style={{ backgroundColor: "#00de3d" }}>DONE!</button>
-      </Btns>
+      <BtnsStatusTask>
+        <DoButton
+          onClick={() => setTaskStatus("do")}
+          doStatus={doStatus}
+          type="button"
+        >
+          DO!
+        </DoButton>
+        <DoingButton
+          type="button"
+          onClick={() => setTaskStatus("doing")}
+          doingStatus={doingStatus}
+        >
+          DOING!
+        </DoingButton>
+        <DoneButton
+          type="button"
+          onClick={() => setTaskStatus("done")}
+          doneStatus={doneStatus}
+        >
+          DONE!
+        </DoneButton>
+      </BtnsStatusTask>
+      <ButtonSave>
+        <button type="button" onClick={() => handleInfoModal(taskStatus)}>
+          Save Changes!
+        </button>
+      </ButtonSave>
     </Form>
   );
 };
