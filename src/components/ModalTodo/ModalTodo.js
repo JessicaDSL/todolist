308830e0dfc519b6f326e4b1f5b43923043d49aa
JessicaLima <jessikurt@gmail.com>
2022-02-23 14:14:18 -0300

    styled: adjuste formatation

--- src/components/ModalTodo/ModalTodo.js
+++ src/components/ModalTodo/ModalTodo.js
@@ -1,24 +1,24 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import TodoForm from "../TodoForm";
-import CancelIcon from '@material-ui/icons/Cancel';
+import CancelIcon from "@material-ui/icons/Cancel";
 
 import { TodoContext } from "../../Context";
-import { Container, Content, Button, ButtonSave } from "./styles";
+import { Container, Content, Button } from "./styles";
 
-const ModalTodo = ({id="modal", task }) => {
-
-  const { closeModal } = useContext(TodoContext)
+const ModalTodo = ({ id = "modal", task }) => {
+  const { closeModal } = useContext(TodoContext);
 
   const handleOutSideClick = (e) => {
-    if(e.target.id === id) closeModal();
-  }
+    if (e.target.id === id) closeModal();
+  };
 
   return (
     <Container id={id} onClick={handleOutSideClick}>
-      <Content >
-        <Button onClick={() => closeModal()}><CancelIcon/></Button>
-        <TodoForm task={task}/>
-        
+      <Content>
+        <Button onClick={() => closeModal()}>
+          <CancelIcon />
+        </Button>
+        <TodoForm task={task} />
       </Content>
     </Container>
   );
