dfc3ff648de6c26c41cfd3212f1d0362ffdec5ec
JessicaLima <jessikurt@gmail.com>
2022-02-23 13:31:27 -0300

    styled: adjuste formatation

--- src/components/TodoForm/styles.js
+++ src/components/TodoForm/styles.js
@@ -2,57 +2,82 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   border-radius: 4px;
-  width: 310px;
+  width: 280px;
   min-height: 220px;
- // background: red;
   font-weight: 700;
   font-size: large;
   z-index: 10;
   border-radius: 16px;
-  padding: 0 .8rem;
+  padding: 0 0.8rem;
   box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.04);
   color: #303030;
+  display: flex;
+  flex-direction: column;
+  textarea {
+    font-size: medium;
+    font-family: "Montserrat Alternates", sans-serif;
+    font-weight: 500;
+    color: #777879;
+    overflow-y: scroll;
+    border-radius: 4px;
+    border: 1px solid #c8cacc;
+  }
 `;
 
-export const Title = styled.div`
-  display: flex;
-  min-height: 2rem;
-  max-height: 4rem;
-  display: flex;
-  align-items: center;
-  overflow-y: scroll;
-  border: 1px solid #c8cacc;
-  border-radius: 4px;
-  width: 82%;
-  padding: 0 0 0 0.4rem;
-  margin: .4rem 0 1.5rem 0;
-  font-size: medium;
-  font-weight: 500;
-  color: #777879;
+export const Title = styled.textarea`
+  padding: 0.5rem;
+  margin: 0.4rem 0 1rem 0;
 `;
 
-export const DescriptionTask = styled.div`
+export const DescriptionTask = styled.textarea`
   margin: 0.4rem 0;
   padding: 0.4rem;
-  border: 1px solid #c8cacc;
-  border-radius: 4px;
   min-height: 8rem;
   max-height: 10rem;
-  overflow-y: scroll;
-  font-weight: 500;
 `;
 
-export const Btns = styled.div`
-width: 100%;
-//background: blue;
-display: flex;
+export const BtnsStatusTask = styled.div`
+  width: 100%;
+  //background: blue;
+  display: flex;
   button {
-    flex-grow: 1; 
+    flex-grow: 1;
     height: 2.2rem;
     border-radius: 6px;
-    margin: 1rem .2rem 1.1rem;
+    margin: 0.8rem 0.2rem 1.1rem;
     color: #fff;
     font-weight: 600;
     border: none;
+    &::disable{
+      background: red;
+    }
   }
+`;
+
+export const ButtonSave = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 0 1rem 0;
+  button {
+    background: #fe4370;
+    border-radius: 6px;
+    border: none;
+    padding: 1rem 1.5rem;
+    color: #fff;
+    font-size: smaller;
+  }
+`;
+
+
+export const DoButton = styled.button`
+background: ${({doStatus}) => doStatus ? "#ff6d54" : 'gray'};
+`
+
+export const DoingButton = styled.button`
+  background: ${({doingStatus}) => doingStatus ? "#00c2ea" : 'gray'};
+`
+
+export const DoneButton = styled.button`
+background: ${({doneStatus}) => doneStatus ? "#00de3d" : 'gray'};
 `
