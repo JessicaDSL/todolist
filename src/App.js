3c6098feda63d660ca9a50407397245456d0e64a
JessicaLima <jessikurt@gmail.com>
2022-03-03 18:59:56 -0300

    feat: add modal Todo on App js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.min.css";
+import ModalTodo from "./components/ModalTodo";
 
 import Home from "./page/Home";
 import GlobalStyle from "./styles/globalStyles";
@@ -10,6 +11,7 @@ const App = () => {
     <div>
       <GlobalStyle />
       <ToastContainer autoClose={3000}/>
+      <ModalTodo />
       <Home />
     </div>
   );
