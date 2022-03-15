6902ca8cd8534c8283ea52e6308366decc669eb5
JessicaLima <jessikurt@gmail.com>
2022-03-15 03:04:28 -0300

    feat: add updateTaks function and refactored function

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { toast } from "react-toastify";
 
 import { v4 as uuidv4 } from "uuid";
@@ -7,7 +7,7 @@ export const TodoContext = createContext({});
 export const TodoProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
 
-  function handleAddTask(title) {
+  function addTask(title) {
     const id = uuidv4();
     title.length === 0
       ? toast.error("Please enter a task!", {
@@ -16,16 +16,30 @@ export const TodoProvider = ({ children }) => {
       : setTasks([...tasks, { id, title }]);
   }
 
-  function handleDeleteTask(id) {
+  function deleteTask(id) {
     setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  function getTasks() {
+    return tasks;
+  }
+
+
+
+  function updateTask(task) {
+    const list = [...tasks];
+    const index = list.findIndex((item) => item.id === task.id);
+    list.splice(index, 1, task);
+    setTasks(list);
   }
 
   return (
     <TodoContext.Provider
       value={{
-        handleAddTask,
-        handleDeleteTask,
-        tasks,
+        addTask,
+        deleteTask,
+        updateTask,
+        getTasks,
       }}
     >
       {children}
